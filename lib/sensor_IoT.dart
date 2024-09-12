c7ef83323a6a5427e3c36dac70f5e5de5aec842e
Vanshbansal99 <vbvansh99@gmail.com>
2024-09-12 13:08:54 +0530

    Update sensor_IoT.dart

--- lib/sensor_IoT.dart
+++ lib/sensor_IoT.dart
@@ -51,7 +51,7 @@ class FirsttPage extends StatelessWidget {
                       onTap:() {
                         //  Navigator.pop(context); 
                           Navigator.of(context).push(
-                         MaterialPageRoute(builder: (context)=> MyApp()) 
+                         MaterialPageRoute(builder: (context)=> MyApp1()) 
                         );
                         // Navigate to Home
                       },
