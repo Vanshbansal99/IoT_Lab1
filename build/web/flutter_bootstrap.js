debce2d65a43b45de72c6f859e65c798c9e51cac
Vanshbansal99 <vbvansh99@gmail.com>
2024-09-12 12:54:19 +0530

    latest5

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2794578515"
+    serviceWorkerVersion: "1260317888"
   }
 });
